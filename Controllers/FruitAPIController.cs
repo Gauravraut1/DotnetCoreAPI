6274e0b9ebbcda759081e9afe448ce0ee5e0af75
Gaurav Raut <gkraut2003@gmail.com>
2026-03-16 00:39:41 +0530

    fruit api added for updating my code

---
+++ Controllers/FruitAPIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreApipractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitAPIController : ControllerBase
+    {
+        public List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Banana",
+            "Cherry",
+            "Date",
+            "Elderberry"
+        };
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            return fruits;
+        }
+    }
+}
